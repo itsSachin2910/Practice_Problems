b3b85d0889b41a8c6db522067bbc80bf743a8b3c
“Sachin” <sachinkumar30690@gmail.com>
2025-01-18 15:40:34 +0530

    ProbelmPractice code

---
+++ ProblemPractice.java
@@ -0,0 +1,18 @@
+public class ProblemPractice{
+    public static void main(String[] args) {
+        System.out.println("Even numbers:");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("\nOdd numbers:");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
